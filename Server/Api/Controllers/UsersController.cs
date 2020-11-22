acfbff588028b577f9ecf118bcfbc357d12a92f8
Luis Marques <luisserafim99@gmail.com>
2020-11-22 05:52:03 +0000

    UsersController

---
+++ Server/Api/Controllers/UsersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller")]
+    public class UsersController : ControllerBase
+    {
+        
+    }
+}
